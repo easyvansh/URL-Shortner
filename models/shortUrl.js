2a554f93b40562ce4366ecfc79f987223ec71c6e
Vansh Singh <vansh2@ualberta.ca>
2024-07-22 18:31:35 +0530

    added shortUrl Schema

---
+++ models/shortUrl.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const shortId = require('shortid');
+
+
+const shortUrlSchema = mongoose.Schema({
+    full:{
+        type: String,
+        require: true
+    },
+    short:{
+        type: String,
+        require: true,
+        default: shortId.generate
+    },
+    Clicks:{
+        type: Number,
+        require: true,
+        default: 0
+    }
+})
+
+module.exports = mongoose.model('ShortUrl', shortUrlSchema)
